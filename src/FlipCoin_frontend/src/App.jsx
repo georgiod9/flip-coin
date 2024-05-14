1d66a2180330d080f6f09b997f0a8a6ba25ed4f2
georgiod9 <georgiod9@gmail.com>
2024-05-14 20:36:03 +0300

    internet identity

--- src/FlipCoin_frontend/src/App.jsx
+++ src/FlipCoin_frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
-import { FlipCoin_backend } from "declarations/FlipCoin_backend";
+import { useEffect, useState } from "react";
+import { FlipCoin_backend, createActor } from "declarations/FlipCoin_backend";
 import Header from "./components/header/header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ControlInterface from "./components/control/ControlInterface";
 import Spacer from "./components/Spacer";
 import Toaster from "./components/Toast/Toaster";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
+import { TopUpComponent } from "./components/TopUp/TopUpComponent";
+
+let actor = FlipCoin_backend;
 
 function App() {
   const [triggerRefresh, setTriggerRefresh] = useState(false);
+  const [walletIdentity, setWalletIdentity] = useState(null);
+  const [identifiedActor, setIdentifiedActor] = useState(null);
+  const [showTopUpModal, setShowTopUpModal] = useState(false);
 
   //Toast vars
   const [toastProps, setToastProps] = useState({
@@ -48,8 +56,66 @@ function App() {
     setTriggerRefresh((prev) => !prev);
   };
 
+  useEffect(() => {
+    console.log(`test`);
+  }, []);
+  useEffect(() => {
+    const connectWallet = async () => {
+      console.log(`Connecting wallet...`);
+
+      try {
+        // Create an auth client
+        let authClient = await AuthClient.create();
+        console.log(`Auth client init`, authClient);
+
+        // Start the login process and wait for it to finish
+        await new Promise((resolve, reject) => {
+          //safari: http://bw4dl-smaaa-aaaaa-qaacq-cai.localhost:4943/
+          //http://bw4dl-smaaa-aaaaa-qaacq-cai.127.0.0.1:4943/
+          //
+          authClient.login({
+            //
+            identityProvider: `http://bw4dl-smaaa-aaaaa-qaacq-cai.localhost:4943/`,
+            onSuccess: resolve,
+            onError: reject,
+          });
+        });
+        console.log(`Logged in!`);
+
+        console.log(`Getting identity..`);
+        const identity = authClient.getIdentity();
+        console.log(`logged in with identity:`, identity);
+
+        // Using the identity obtained from the auth client, create an agent to interact with the IC.
+        const agent = new HttpAgent({ identity });
+        // Using the interface description of our webapp, create an actor that we use to call the service methods.
+        actor = createActor(process.env.CANISTER_ID_FLIPCOIN_BACKEND, {
+          agent,
+        });
+
+        setWalletIdentity(identity);
+        setIdentifiedActor(actor);
+
+        console.log(`Created new actor from identified agent.`);
+      } catch (error) {
+        console.error("Error during wallet connection:", error);
+      }
+
+      return false;
+    };
+
+    connectWallet();
+  }, []);
+
   return (
     <div>
+      {showTopUpModal && (
+        <TopUpComponent showControl={[showTopUpModal, setShowTopUpModal]} />
+      )}
+      <Header
+        refreshControl={[triggerRefresh, setTriggerRefresh]}
+        setShowTopUpModal={setShowTopUpModal}
+      />
       {showToaster && (
         <Toaster
           headerContent={toastProps.toastHeader}
@@ -61,11 +127,10 @@ function App() {
           onHide={handleToastHide}
         />
       )}
-      <div>
-        <Header refreshControl={[triggerRefresh, setTriggerRefresh]} />
-      </div>
+
       <div>
         <ControlInterface
+          backendActor={actor}
           callToaster={callToaster}
           toggleRefresh={toggleRefresh}
           refreshControl={[triggerRefresh, setTriggerRefresh]}
