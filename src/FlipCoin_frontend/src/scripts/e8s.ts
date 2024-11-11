6547c159f1da8be5577f17d97fd5e49128970203
georgiod9 <georgiod9@gmail.com>
2024-11-11 20:05:51 +0300

    convert to ts file

--- src/FlipCoin_frontend/src/scripts/e8s.ts
+++ src/FlipCoin_frontend/src/scripts/e8s.ts
@@ -1,11 +1,11 @@
 export const e8s = 100000000n; // Use BigInt for e8s
 
-export const e8sToIcp = (amountInE8s) => {
+export const e8sToIcp = (amountInE8s: number | bigint) => {
     const icpAmount = Number(amountInE8s) / Number(e8s);
     return icpAmount;
 }
 
-export const icpToE8s = (amountInIcp) => {
+export const icpToE8s = (amountInIcp: number) => {
     const e8sAmount = BigInt(Math.round(amountInIcp * Number(e8s)));
     return e8sAmount;
 }
